c271d109799280e029b93862880c475630508edf
Ali Nematollahi <ali.nematollahi2020@gmai.com>
2025-06-04 19:58:07 +0200

    Implemented homework assignment

---
+++ app/about_us/OurPartners.js
@@ -0,0 +1,25 @@
+const OurPartners = () => {
+  // TASK - React 1 week 1
+  // Create the "Our Crew section"
+  // Use the descriptions provided in /app/about_us/README.md
+  // Use the pictures from /public/crew
+  // Some inspiration ideas found in /data/inspiration_about_us
+  return (
+    <>
+      <p>
+        We collaborate with some of the most respected names in the space and
+        technology industries to make every journey extraordinary.
+      </p>
+      <img src="/business_partners/alphabet-logo.png" />
+      <img src="/business_partners/amazon_logo.png" />
+      <img src="/business_partners/CBC_Logo_White.png" />
+      <img src="/business_partners/Microsoft-Logo-white.png" />
+      <img src="/business_partners/nyu-logo.png" />
+      <img src="/business_partners/QueensLogo_white.png" />
+      <img src="/business_partners/samsung-logo.png" />
+      <img src="/business_partners/sodexo-logo.png" />
+    </>
+  );
+};
+
+export default OurPartners;
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Navbar } from "@/components/ui/Navbar";
 import { Footer } from "@/components/ui/Footer";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -20,6 +21,7 @@ export const RootLayout = ({ children }) => {
         {/* TASK - React 1 week 1 */}
         {/* Import and use the Footer component here */}
         {/* Footer found in the ui/Footer.js folder */}
+        <Footer />
       </body>
     </html>
   );
